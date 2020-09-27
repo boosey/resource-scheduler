e251c90f5f1615463fd0a41304f8d144ddae6a1f
Boosey <john.boudreaux@us.ibm.com>
2020-09-26 23:48:52 -0500

    Initial service

---
+++ resource-commands/src/main/java/boosey/ResourceCommands.java
@@ -0,0 +1,19 @@
+package boosey;
+
+import io.quarkus.funqy.Context;
+import io.quarkus.funqy.Funq;
+import io.quarkus.funqy.knative.events.CloudEvent;
+import io.quarkus.funqy.knative.events.CloudEventMapping;
+import org.jboss.logging.Logger;
+
+public class ResourceCommands {
+    private static final Logger log = Logger.getLogger(ResourceCommands.class);
+
+    @Funq
+    @CloudEventMapping(trigger = "addResource", responseSource = "handleAddResource", responseType = "addedResource")
+    public String handleAddResource(String input, @Context CloudEvent event) {
+        log.info("*** add resource ***");
+        return input + "::" + "annotatedChain";
+    }
+
+}
