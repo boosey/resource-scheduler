c3586471d66200a987f51ecf4101bbaa9184ffc5
Boosey <john.boudreaux@us.ibm.com>
2020-11-08 18:46:21 -0600

    Moved Owner Events Grpc to Interfaces Module

--- owner/owner-command-event-processor/src/main/java/boosey/OwnerCommandEventProcessor.java
+++ owner/owner-command-event-processor/src/main/java/boosey/OwnerCommandEventProcessor.java
@@ -8,9 +8,9 @@ import io.quarkus.funqy.Funq;
 import io.quarkus.funqy.knative.events.CloudEvent;
 import io.quarkus.funqy.knative.events.CloudEventMapping;
 import io.quarkus.grpc.runtime.annotations.GrpcService;
-import lombok.extern.slf4j.Slf4j;
+// import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+// @Slf4j
 public class OwnerCommandEventProcessor {
 
     private static Gson gson = new Gson();
@@ -22,13 +22,6 @@ public class OwnerCommandEventProcessor {
     @Funq
     @CloudEventMapping(trigger = "ADD_OWNER")
     public void handleAddOwner(Owner owner, @Context CloudEvent eventContext) {
-
-        // val e = new ResourceSchedulerEvent<Owner>(
-        //     Type.OWNER_ADDED,
-        //     Source.HANDLE_ADD_OWNER,
-        //     owner);
-
-        // events.fire(e);
         
         grpcEvents.fire(FireRequest.newBuilder()
                         .setType(EventType.OWNER_ADDED)
@@ -42,13 +35,6 @@ public class OwnerCommandEventProcessor {
     @CloudEventMapping(trigger = "REPLACE_OWNER")
     public void handleReplaceOwner(Owner owner, @Context CloudEvent eventContext) {
 
-        // val e = new ResourceSchedulerEvent<Owner>(
-        //     Type.OWNER_REPLACED,
-        //     Source.HANDLE_REPLACE_OWNER,
-        //     owner);
-            
-        // events.fire(e);
-
         grpcEvents.fire(FireRequest.newBuilder()
                         .setType(EventType.OWNER_REPLACED)
                         .setSource(EventSource.HANDLE_REPLACE_OWNER)
@@ -60,13 +46,6 @@ public class OwnerCommandEventProcessor {
     @CloudEventMapping(trigger = "DELETE_ALL_OWNERS")
     public void handleDeleteAllOwners(NoEventData eventData, @Context CloudEvent eventContext) {
 
-        // log.info("handling delete all event");
-        // val e = new ResourceSchedulerEvent<NoEventData>(
-        //     Type.ALL_OWNERS_DELETED,
-        //     Source.HANDLE_DELETE_ALL_OWNERS,
-        //     NoEventData.builder().build());
-            
-        // events.fire(e);
         grpcEvents.fire(FireRequest.newBuilder()
                 .setType(EventType.ALL_OWNERS_DELETED)
                 .setSource(EventSource.HANDLE_DELETE_ALL_OWNERS)
@@ -77,14 +56,6 @@ public class OwnerCommandEventProcessor {
     @Funq
     @CloudEventMapping(trigger = "DELETE_OWNER")
     public void handleDeleteOwner(ItemIdData ownerId, @Context CloudEvent evtCtx) {
-
-        log.info("in command.handleDeleteOwner: " + ownerId);
-        // val e = new ResourceSchedulerEvent<ItemIdData>(
-        //     Type.OWNER_DELETED,
-        //     Source.HANDLE_DELETE_OWNER,
-        //     ownerId);
-            
-        // events.fire(e);
 
         grpcEvents.fire(FireRequest.newBuilder()
                 .setType(EventType.OWNER_DELETED)
